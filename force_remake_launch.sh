9506d2a3c4057645588ef260e551a3f498b0efe3
Magnus Gärtner <magnusgaertner@gmail.com>
2017-11-29 13:50:01 +0100

    modified scripts

---
+++ force_remake_launch.sh
@@ -0,0 +1,6 @@
+rm -rf catkin_ws/devel
+rm -rf catkin_ws/logs
+rm -rf catkin_ws/build
+catkin clean
+
+./launch.sh
--- setup.sh
+++ setup.sh
@@ -1,3 +1,23 @@
+#install ros
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+
+sudo apt-get update
+
+sudo apt-get install ros-kinetic-desktop
+
+sudo rosdep init
+
+rosdep update
+
+echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
+
+source ~/.bashrc
+
+sudo apt-get install python-rosinstall python-rosinstall-generator python-wstool build-essential
+
+
 #install catkin command line tools
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu `lsb_release -sc` main" > /etc/apt/sources.list.d/ros-latest.list'
 wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
@@ -6,8 +26,4 @@ sudo apt-get install python-catkin-tools
 
 #install additional packages needed
 sudo apt-get install ros-kinetic-rqt-multiplot
-
-#build all catkin packages
-cd catkin_ws/
-catkin build
 
