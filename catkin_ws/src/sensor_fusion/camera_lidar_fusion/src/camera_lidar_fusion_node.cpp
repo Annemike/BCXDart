fbb4386ab5a2a388d699e0cbe380d23f5da1377a
Magnus Gärtner <magnusgaertner@gmail.com>
2018-01-18 03:12:18 +0100

    first version of cone to image projection working

--- catkin_ws/src/sensor_fusion/camera_lidar_fusion/src/camera_lidar_fusion_node.cpp
+++ catkin_ws/src/sensor_fusion/camera_lidar_fusion/src/camera_lidar_fusion_node.cpp
@@ -12,6 +12,7 @@
 #include <cv_bridge/cv_bridge.h>
 
 #include <message_filters/sync_policies/approximate_time.h>
+#include <message_filters/synchronizer.h>
 
 #include <chrono>
 #include "dart_msgs/cv_cone_list.h"
@@ -19,12 +20,13 @@
 #include "dart_msgs/cone_list.h"
 #include "sensor_msgs/CameraInfo.h"
 #include "dart_msgs/point2d_list.h"
-
+#include <image_geometry/pinhole_camera_model.h>
 #include "std_msgs/Int32MultiArray.h"
 
 #include <opencv/cv.hpp>
-
-
+#include <message_filters/subscriber.h>
+#include "tf/transform_datatypes.h"
+#include <tf/transform_listener.h>
 using namespace cv;
 using namespace std;
 
@@ -33,47 +35,98 @@ ros::Publisher pub;
 ros::Subscriber camera_info_sub;
 ros::Subscriber lidar_cone_sub;
 cv::Mat K;
+image_geometry::PinholeCameraModel cam_model_;
+tf::TransformListener* TFlistener;
 
-static void projectPoint3D(dart_msgs::cone_list::ConstPtr lidarCones){
 
-    if(lidarCones->cones.size()<=0){
+ros::Publisher annotated_cones_cones;
+
+static void fuseCones(const dart_msgs::cone_list::ConstPtr& lidarCones){
+    if(lidarCones->cones.size()<=0 || !cam_model_.initialized()){
         return;
     }
-    std::vector<cv::Point3f> objVector;
+
+
+    tf::StampedTransform transform;
+    try{
+
+        TFlistener->lookupTransform("/ZED_left_camera", "/velodyne",
+                                 ros::Time(0), transform);
+    }
+    catch (tf::TransformException ex){
+        ROS_ERROR("%s",ex.what());
+        ros::Duration(1.0).sleep();
+    }
+
+    std::vector<cv::Point2f> projectedPoints;
+    //std::vector<cv::Point3f> objVector;
     for(const dart_msgs::cone& cone : lidarCones->cones){
 
-        cv::Point3f obj(cone.position.x, cone.position.y, cone.position.z);
-
-        objVector.push_back(obj);
+        tf::Point point(cone.position.x, cone.position.y, cone.position.z);
+        point = transform * point;
+        cv::Point3f obj(-point.y(), -point.z(), point.x());
+        if(obj.z>0) {
+            projectedPoints.push_back(cam_model_.project3dToPixel(obj));
+        }
+        //objVector.push_back(obj);
     }
-    std::vector<cv::Point2f> projectedPoints;
 
-    cv::projectPoints(objVector, Mat::zeros(1,3,CV_64F), Mat::zeros(1,3,CV_64F), K,Mat::zeros(1,4,CV_64F),  projectedPoints);
+    ROS_ERROR("projecting...");
+
+    //cv::projectPoints(objVector, Mat::zeros(1,3,CV_64F), Mat::zeros(1,3,CV_64F), K,Mat::zeros(1,4,CV_64F),  projectedPoints);
 
     dart_msgs::point2d_list points;
 
+    dart_msgs::cone_list annotated_cones;
 
-    for(const cv::Point2f& point : projectedPoints){
+    for(int i =0; i<projectedPoints.size();i++){
+        cv::Point2f point = projectedPoints[i];
         geometry_msgs::Point dart_point;
         dart_point.x = point.x;
         dart_point.y = point.y;
         points.points.push_back(dart_point);
+        /*
+        for(const dart_msgs::cv_cone box:cv_cones->cones){
+            if(std::abs(box.x-point.x)<=(box.width/2) && std::abs(box.y-point.y)<=(box.height/2) ){
+                dart_msgs::cone annotatedCone = lidarCones->cones[i];
+                annotatedCone.type = box.type;
+                annotated_cones.cones.push_back(annotatedCone);
+                break;
+            }
+        }
+         */
+
     }
     pub.publish(points);
-
-
 }
 
 static void cameraInfoCallback(sensor_msgs::CameraInfo::ConstPtr info){
     K = cv::Mat(3, 3, CV_32F, info->K[0]);
+
     camera_info_sub.shutdown();
-}
+    cam_model_.fromCameraInfo(info);
+    tf::Point testpoint(2,0,0);
 
+    tf::StampedTransform transform;
+    try{
 
-void readParams(ros::NodeHandle &n) {
-
-    K = (Mat_<float>(3, 3) << 699.943, 0.0, 650.156, 0.0, 699.943, 383.336, 0.0, 0.0, 1.0);
-
+        TFlistener->lookupTransform("/ZED_left_camera", "/velodyne",
+                                    ros::Time(0), transform);
+    }
+    catch (tf::TransformException ex){
+        ROS_ERROR("%s",ex.what());
+        ros::Duration(1.0).sleep();
+    }
+    testpoint = transform*testpoint;
+    cv::Point3f cvPt(-testpoint.y(), -testpoint.z(), testpoint.x());
+    std::cout<<"point:";
+    std::cout<<testpoint;
+    std::cout<<"transformed point:";
+    std::cout<<cvPt;
+    std::cout<<"P matrix:";
+    std::cout<<cam_model_.fullProjectionMatrix();
+    std::cout<<"projected testpoint";
+    std::cout<<cam_model_.project3dToPixel(cvPt);
 }
 
 
@@ -81,17 +134,17 @@ int main(int argc, char **argv) {
 
     ros::init(argc, argv, "camera_lidar_fusion_node");
     ros::NodeHandle nh;
-    readParams(nh);
+
+    TFlistener = new tf::TransformListener();
 
     camera_info_sub = nh.subscribe("/left/camera_info_sub", 1, cameraInfoCallback);//TODO
-    lidar_cone_sub = nh.subscribe("/lidar_cones", 1, projectPoint3D);
+    lidar_cone_sub = nh.subscribe("/lidar_cones", 1, fuseCones);
 
     pub = nh.advertise<dart_msgs::point2d_list>("projected_cones", 100);
+    annotated_cones_cones = nh.advertise<dart_msgs::cone_list>("fused_cones", 10);
 
-    cout << "started\n";
-
+    ROS_INFO("cone_fusion started");
     ros::spin();
-
     return 0;
 }
 
